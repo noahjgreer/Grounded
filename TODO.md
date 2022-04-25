20917b031b606550608fefc17d54789b7c564d59
Noah Greer <noahjgreer@gmail.com>
2022-04-24 22:25:16 -0700

    v0.7.4
    Created Audio Database
    Created Template Article Page

--- TODO.md
+++ TODO.md
@@ -5,3 +5,12 @@
 - [ ] Revamp Status Text on Newsletter Submission
 - [ ] Make "Ask" page send Responses in a JSON to a Server
 - [ ] Make a gallery so you can view submitted questions on the question wall.
+
+
+## Important Blocks
+- [ ] Redo Button Animation to make them less annoying
+- [ ] Create an example for an episode/article page.
+- [ ] !! Give the about section on the index.html actual text, not lorem ipsum! 👀
+- [ ] Make Episodes Page
+- [ ] Make About Page
+- [ ] Fix Footer (Make it appealing)
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -5,11 +5,20 @@ async function getQuestions() {
 
     // Write questions to page.
     var questionsHTML = "";
+
+    var noName = function() {
+        if (questionRecieve.questions[i].name == undefined) {
+            return "";
+        } else {
+            return questionRecieve.questions[i].name;
+        }
+    }
+
     for (var i = 0; i < questionRecieve.questions.length; i++) {
         var questionsHTML = questionsHTML + `
             <div>
                 <h2>${questionRecieve.questions[i].question}</h2>
-                <h3>${questionRecieve.questions[i].name}</h3>
+                <h3>` + noName() + `</h3>
                 <time>${questionRecieve.questions[i].date}</time>
                 <button class="icon">
                 <img src="/assets/icons/like.svg">
