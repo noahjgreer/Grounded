8e010578cecac944e3051ad8afab0f03d2807067
Noah Greer <noahjgreer@gmail.com>
2022-04-26 15:20:40 -0700

    v0.7.8
    Finished Audio Player Element

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,17 +1,58 @@
 var player = document.getElementById('player').innerHTML;
 var playButton = document.getElementsByClassName('play')[0];
 var audio = document.getElementsByTagName('audio')[0];
-var playStatus = false;
+var currentAudioPosition;
+var playbackBarContainer = document.querySelector('.playback-container');
+var playbackBar = document.querySelector('.playback');
+var pauseStatus = audio.paused;
+
+audio.addEventListener('timeupdate', updateProgress)
+audio.addEventListener('ended', endEvents)
+audio.addEventListener('pause', audioPaused)
+audio.addEventListener('play', audioPlay)
+playbackBarContainer.addEventListener('click', setProgress)
+
+
+function audioPlay(e) {
+    playButton.style["background-image"] = "url(/assets/icons/pause.svg)"
+}
+
+function audioPaused(e) {
+    playButton.style["background-image"] = "url(/assets/icons/play.svg)"
+}
+
+function endEvents(e) {
+    audioPaused();
+}
+
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    currentAudioPosition = (currentTime / duration) * 100;
+
+    playbackBar.style.width = `${currentAudioPosition}%`
+}
+
+function setProgress(e) {
+    var width = this.clientWidth;
+    var clickX = e.offsetX;
+    var duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
 
 function togglePlay() {
-    if (playStatus == false) {
+
+    if (pauseStatus == false) {
+        audio.pause();
+        playButton.style["background-image"] = "url(/assets/icons/play.svg)"
+
+        pauseStatus = true;
+    } else {
         audio.play();
         playButton.style["background-image"] = "url(/assets/icons/pause.svg)"
 
-        playStatus = true;
-    } else {
-        audio.pause();
-        playButton.style["background-image"] = "url(/assets/icons/play.svg)"
-        playStatus = false;
+
+        pauseStatus = false;
     }
 }
